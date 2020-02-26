ee788dab815ad6efd67ab2e753e6d878e09dc30c
Jan Vaverka <honzavaverka1996@gmail.com>
2020-02-26 20:49:50 +0100

    26.2.2020

---
+++ lib/services/auth.dart
@@ -0,0 +1,31 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user obj based on FirebaseUser
+
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // sign in anon
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email & password
+
+  // register with email & password
+
+  // sign out
+
+}
